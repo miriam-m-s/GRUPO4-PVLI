6f475b7c4e4029927d1ef5ef76dee432da1622c5
MIRIAMMARTIN <mirima10@ucm.es>
2021-10-13 20:49:47 +0200

    JUEGO DE GUILLE

---
+++ simplephasergame-main/README.md
@@ -0,0 +1,3 @@
+# Juego simple con Phaser
+
+Este juego ha sido desarrollado por Carlos León Aznar (<https://github.com/cleongh/simplephasergame>). En este repositorio solo se ha hecho una reorganización del código y de los assets usados para su desarrollo.
---
+++ simplephasergame-main/src/game.js
@@ -0,0 +1,29 @@
+import Boot from './boot.js';
+import End from './end.js';
+import Platform from './platform.js';
+import Level from './scene.js';
+
+/**
+ * Inicio del juego en Phaser. Creamos el archivo de configuración del juego y creamos
+ * la clase Game de Phaser, encargada de crear e iniciar el juego.
+ */
+let config = {
+    type: Phaser.AUTO,
+    width:  1000,
+    height: 500,
+    scale: {
+        // mode: Phaser.Scale.FIT,  
+        autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
+    },
+    pixelArt: true,
+    scene: [Boot, Level, End],
+    physics: { 
+        default: 'arcade', 
+        arcade: { 
+            gravity: { y: 400 }, 
+            debug: false 
+        } 
+    }
+};
+
+new Phaser.Game(config);
