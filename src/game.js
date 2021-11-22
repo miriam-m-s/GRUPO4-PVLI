8a9f3b7798a7cbc8bbb5aae30b2c1bb3b02dc224
Jacobo0601 <jacoboal@ucm.es>
2021-11-22 12:35:53 +0100

    HTML Overlay (temporal)

--- src/game.js
+++ src/game.js
@@ -11,8 +11,8 @@ let config =
     height: 350,
     roundPixels: true,
     scale: {
-        //mode: Phaser.Scale.FIT,  
-        //autoCenter: Phaser.Scale.CENTER_BOTH,
+        //mode: Phaser.Scale.NONE,  
+        autoCenter: Phaser.Scale.CENTER_BOTH,
         
     },
     zoom: 2.1,
---
+++ src/gui.js
@@ -0,0 +1,22 @@
+
+/**
+ * @extends Phaser.Scene
+ */
+
+ export default class GUI extends Phaser.Scene {
+
+
+constructor() 
+{
+  super({ key: 'level' });
+}
+
+//Creación de los elementos de la escena principal de juego
+
+create() 
+{
+    console.log("GUI");
+    var textConfig={fontSize:'20px',color:'#ff0000',fontFamily: 'Arial'};
+    this.add.text(game.config.width/2,game.config.height/2,textConfig,"SomeText",textConfig);    
+}
+ }
