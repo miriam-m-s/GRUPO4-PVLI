b869ebac31cc3acbcfd2858bce134f5609aa612c
Jacobo0601 <jacoboal@ucm.es>
2021-12-14 09:17:19 +0100

    Html update to Main (no imgs)

--- src/game.js
+++ src/game.js
@@ -1,28 +1,29 @@
 import Boot from './boot.js';
 import End from './end.js';
 import Platform from './platform.js';
-import MainMenuScene from './mainMenuScene.js';
 import Level from './scene.js';
+import MainMenuScene from './mainMenuScene.js';
 
-let config = {
+let config = 
+{
     type: Phaser.WEBGL,
     //NES aspect ratio es 8:7
-    width: 400,
+    width:  400,
     height: 350,
     roundPixels: true,
     scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-
+        parent: 'mygame',
+        autoCenter: Phaser.Scale.CENTER_BOTH
+        //autoCenter: Phaser.Scale.CENTER_BOTH,
     },
-    zoom: 2.1,
+    zoom: 1.4,
     pixelArt: true,
     scene: [Boot, MainMenuScene, Level, End],
-    physics: {
-        default: 'arcade',
-        arcade: {
-            debug: true
-        }
+    physics: { 
+        default: 'arcade', 
+        arcade: { 
+            debug: false
+        } 
     },
     audio: {
         disableWebAudio: true
