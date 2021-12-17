85a6799ae27ba51eb958a8ea03fd78f387d8e70d
miriam-m-s <81752837+miriam-m-s@users.noreply.github.com>
2021-12-17 01:22:03 +0100

    gh update

---
+++ src/intro.js
@@ -0,0 +1,21 @@
+export default class Intro extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'intros'
+        });
+    }
+
+
+    
+
+    create() {
+        this.intro = this.add.video(200, 100, 'vid');
+        this.intro.setScale(0.3);
+        this.intro.play(true);
+    }
+    update() {
+         if (this.intro.getProgress() >= 0.99) {
+             this.scene.start('mainMenu');
+         }
+    };
+}
