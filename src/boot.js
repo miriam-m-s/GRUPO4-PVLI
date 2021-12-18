66f74024b454f625082ea92d02256c48c11b6712
Jacobo0601 <jacoboal@ucm.es>
2021-12-18 19:01:27 +0100

    Map02

--- src/boot.js
+++ src/boot.js
@@ -117,7 +117,7 @@ export default class Boot extends Phaser.Scene {
 
     this.load.setPath('assets/maps/');
     // Carga de Tilemap
-    this.load.tilemapTiledJSON('tilemap01', 'Map01.json');
+    this.load.tilemapTiledJSON('tilemap01', 'Map02.json');
     // Carga de spritesheet
     this.load.image('mapSpriteSheet', 'spritesheets/mansionNes.png');
   }
