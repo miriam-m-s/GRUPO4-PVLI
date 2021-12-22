e58258b2a9909554362cc3e04f3bebb59f03eec8
NachoDelCastillo <nachodelcastillorubio@gmail.com>
2021-12-22 11:50:32 +0100

    level1 fixed

--- src/boot.js
+++ src/boot.js
@@ -303,6 +303,6 @@ export default class Boot extends Phaser.Scene {
     //Level creation
 
    // this.scene.start('intros');
-    this.scene.start('level6');
+    this.scene.start('level1');
   }
 }
